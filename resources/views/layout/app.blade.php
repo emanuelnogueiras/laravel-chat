e8d699cd273925a695ed1715dcea3e9ce9f3e64e
Emanuel Nogueiras <emanuelnogueiras@gmail.com>
2020-07-10 01:03:54 +0200

    nueva version pusher

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,11 +11,11 @@
     <link href="https://fonts.googleapis.com/css?family=Baloo+2|PT+Sans&display=swap" rel="stylesheet">
     <style>
       html, body{
-        font-family: 'PT Sans', sans-serif;        
+        font-family: 'PT Sans', sans-serif;
       }
 
       input.form-control{
-          
+
       }
 
 
@@ -26,23 +26,23 @@
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous"></script>
 
-    <script src="https://js.pusher.com/5.1/pusher.min.js"></script>    
-    
+    <script src="https://js.pusher.com/6.0/pusher.min.js"></script>
+
     <title>Test</title>
-    
+
     @livewireStyles
-    @livewireScripts    
+    @livewireScripts
 
   </head>
   <body>
-    
+
     @yield("contenido")
 
-    
+
 
     <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->      
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
